5ad31d4707bff041ce3a1b650970b1d24362caf6
Priyansh-commits-3005 <117034550+Priyansh-commits-3005@users.noreply.github.com>
2023-12-17 17:12:36 +0530

    adding the level scenes

--- Assets/scripts/Oscillator.cs
+++ Assets/scripts/Oscillator.cs
@@ -7,7 +7,8 @@ public class Oscillator : MonoBehaviour
     //variables
     Vector3 startingPosition;
     [SerializeField]Vector3 movementVector;
-    [SerializeField] [Range(0, 1)] float movementFactor;
+    float movementFactor;
+    [SerializeField]float period = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +19,15 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if(period == 0f) { return; }//this would work but for more accuracy
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period;//conitnously increasing
+
+        const float tau = Mathf.PI * 2;//constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau);//going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f;  //recalculating sin wave so that its from 0 to 1
+        
         Vector3 offset = movementVector*movementFactor;
         transform.position = startingPosition + offset;
     }
